c173b6c495768549b9cae4d61ce428c8421c53f4
JuanDlc <juanjuancuevas11@gmail.com>
2025-09-10 23:36:20 -0600

    Corregido, cambie resta por multiplacion

--- helloGit3.ts
+++ helloGit3.ts
@@ -2,9 +2,9 @@ const suma = (num1: number, num2: number) => {
     return num1 + num2;
 }
 
-const resta = (num1: number, num2: number) => {
-    return num1 - num2;
+const multi = (num1: number, num2: number) => {
+    return num1 * num2;
 }
 
 console.log(suma(3,5));
-console.log(resta(4,6));
+console.log(multi(4,6));
