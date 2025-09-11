311a8eb607f95db890b43c831a66fba62cc704b0
JuanDlc <juanjuancuevas11@gmail.com>
2025-09-10 23:58:04 -0600

    Ya esta todo arreglado

--- helloGit3.ts
+++ helloGit3.ts
@@ -2,19 +2,9 @@ const suma = (num1: number, num2: number) => {
     return num1 + num2;
 }
 
-<<<<<<< HEAD
-const division = (num1: number, num2: number) => {
-    if (num2 == 0) return 0
-    return num1 - num2;
-}
-
-console.log(suma(3,5));
-console.log(division(4,6));
-=======
 const multi = (num1: number, num2: number) => {
     return num1 * num2;
 }
 
 console.log(suma(3,5));
 console.log(multi(4,6));
->>>>>>> master
